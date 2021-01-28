9cbff90973f2290a6ba4d5ea91df381cfd563535
oduvan <a.lyabah@checkio.org>
2021-01-28 09:30:16 +0200

    first version

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -8,10 +8,77 @@ requirejs(['ext_editor_io', 'jquery_190'],
         var io = new extIO({
             multipleArguments: true,
             functions: {
-                python: 'sum_two',
-                js: 'sumTwo'
+                python: 'sum_light',
+                js: 'sumLight'
             }
         });
+        io.parseInputArguments = function(data) {
+            var fname = '';
+            var is_py = this.getLanguage() == "python"
+            if (is_py) {
+                fname = 'datetime';
+            } else {
+                fname = 'new Date';
+            }
+            var els = data[0];
+            els = els.map(function(ee) {
+                if (ee.length === 2) {
+                    return '[' + fname + '(' + ee[0].join(', ') + '), ' + ee[1] + ']'
+                } else {
+                    return fname + '(' + ee.join(', ') + ')'    
+                }
+                
+            })
+            els = '[\n' + els.join(',\n') + '\n]'
+            if (is_py) {
+                els = 'els=' + els;
+            }
+
+            var start_watching = '';
+            var end_watching = '';
+            var operating = '';
+            var req = '';
+
+            if (is_py) {
+                if (data.length > 1 && data[1]) {
+                    start_watching = ',\nstart_watching=' + fname + '(' + data[1].join(', ') +')';
+                }
+                if (data.length > 2 && data[2]) {
+                    end_watching = ',\nend_watching=' + fname + '(' + data[2].join(', ') +')';
+                }
+                if (data.length > 3 && data[3]) {
+                    operating = ',\noperating=timedelta(seconds=' + data[3] +')';
+                }
+                if (data.length > 4 && data[4]) {
+                    req = ',\nreq=' + data[4];
+                }
+            } else {
+                if (data.length > 1) {
+                    if (data[1]){
+                        start_watching = ',\n' + fname + '(' + data[1].join(', ') +')';
+                    } else {
+                        start_watching = ',\n' + data[1];
+                    }
+                }
+                if (data.length > 2) {
+                    if (data[2]){
+                        end_watching = ',\n' + fname + '(' + data[2].join(', ') +')';
+                    } else {
+                        end_watching = ',\n' + data[2];
+                    }
+                }
+                if (data.length > 3) {
+                    operating = ',\n' + data[3];
+                }
+                if (data.length > 4) {
+                    req = ',\n' + data[4];
+                }
+            }
+
+            
+
+            return els + start_watching + end_watching + operating;
+        }
         io.start();
     }
 );
--- verification/referee.py
+++ verification/referee.py
@@ -33,16 +33,58 @@ from checkio.referees import cover_codes
 
 from tests import TESTS
 
+all_datetime_py = '''
+def cover(func, in_data):
+    from datetime import datetime, timedelta
+    els = [((datetime(*el[0]), el[1]) if len(el) == 2 else datetime(*el)) for el in in_data[0]]
+    kwargs = {}
+    if len(in_data) > 1 and in_data[1] is not None:
+        kwargs['start_watching'] = datetime(*in_data[1])
+    if len(in_data) > 2 and in_data[2] is not None:
+        kwargs['end_watching'] = datetime(*in_data[2])
+    if len(in_data) > 3 and in_data[3] is not None:
+        kwargs['operating'] = timedelta(seconds=in_data[3])
+    if len(in_data) > 4 and in_data[4] is not None:
+        kwargs['req'] = in_data[4]
+    return func(els, **kwargs)
+'''
+
+all_datetime_js = '''
+function cover(func, in_data) {
+    var els = in_data[0].map(function(ee) {
+        if (ee.length === 2) {
+            return [new Date(ee[0][0], ee[0][1], ee[0][2], ee[0][3], ee[0][4], ee[0][5]), ee[1]];
+        } else {
+            return new Date(ee[0], ee[1], ee[2], ee[3], ee[4], ee[5]);
+        }
+    });
+    var start_watching, end_watching, operating, req;
+    if (in_data.length > 1 && in_data[1]) {
+        start_watching = new Date(in_data[1][0], in_data[1][1], in_data[1][2], in_data[1][3], in_data[1][4], in_data[1][5]);
+    }
+    if (in_data.length > 2 && in_data[2]) {
+        end_watching = new Date(in_data[2][0], in_data[2][1], in_data[2][2], in_data[2][3], in_data[2][4], in_data[2][5]);
+    }
+    if (in_data.length > 3 && in_data[3]) {
+        operating = in_data[3];
+    }
+    if (in_data.length > 4 && in_data[4]) {
+        req = in_data[4];
+    }
+    return func(els, start_watching, end_watching, operating, req);
+}
+'''
+
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "sum_two",
-            "js": "sumTwo"
+            "python": "sum_light",
+            "js": "sumLight"
         },
         cover_code={
-            'python-3': cover_codes.unwrap_args,
-            'js-node': cover_codes.js_unwrap_args
+            'python-3': all_datetime_py,
+            'js-node': all_datetime_js
         }
     ).on_ready)
